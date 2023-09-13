8de1aceddc43d2bff3a919db8cb7f1fa54d73316
SriVarshan733 <srivarshan.it20@bitsathy.ac.in>
2023-09-13 09:43:57 +0530

    new script to update data in products table

--- Python/phpdb.py
+++ Python/phpdb.py
@@ -7,7 +7,6 @@ user = "root"
 password = ""
 database = "kk"
 
-
 def connect():
     try:
         connection = mysql.connector.connect(
@@ -18,31 +17,36 @@ def connect():
     except mysql.connector.Error as e:
         print("Error:", e)
 
-
 def getListOfProductId(conn):
     cursor = conn.cursor()
-    query = "SELECT DISTINCT product_id FROM bids"
+    query = "SELECT DISTINCT product_id FROM bids WHERE status = 2"
     cursor.execute(query)
     results = cursor.fetchall()
     return [product[0] for product in results]
 
+def updateProductsTable(conn, product_id, buyer_id, bid_amt):
+    cursor = conn.cursor()
+    update_query = f"UPDATE products SET buyer_id = {buyer_id}, bid_amt = {bid_amt} WHERE id = {product_id}"
+    cursor.execute(update_query)
+    conn.commit()
 
 def checkChangeTheStatus(conn, listOfProductId):
     cursor = conn.cursor()
 
     for product_id in listOfProductId:
-        # Get the highest bid_amount for the product_id
-        highest_bid_query = f"SELECT MAX(bid_amount) FROM bids WHERE product_id = {product_id}"
+        # Get the highest bid_amount and buyer_id for the product_id where status = 2
+        highest_bid_query = f"SELECT MAX(bid_amount), user_id FROM bids WHERE product_id = {product_id} AND status = 2"
         cursor.execute(highest_bid_query)
-        highest_bid = cursor.fetchone()[0]
-
-        # Update the status to 2 only for the highest bid_amount
-        update_query = f"UPDATE bids SET status = 2 WHERE product_id = {product_id} AND bid_amount = {highest_bid}"
-        cursor.execute(update_query)
-        conn.commit()
+        result = cursor.fetchone()
+        
+        if result:
+            highest_bid, buyer_id = result
+            updateProductsTable(conn, product_id, buyer_id, highest_bid)
+            print(f"Updated product {product_id}: buyer_id={buyer_id}, bid_amt={highest_bid}")
+        else:
+            print(f"No valid bid found for product {product_id}")
 
     print("Data Updated Successfully.")
-
 
 connection = connect()
 listOfProductId = getListOfProductId(connection)
