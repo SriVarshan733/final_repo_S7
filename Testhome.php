a3eeb65ce3bd2234d3faa75dcc3f9b53b90d8add
SriVarshan733 <srivarshan.it20@bitsathy.ac.in>
2023-09-14 15:28:31 +0530

    test

--- Testhome.php
+++ Testhome.php
@@ -1,22 +1,6 @@
 <?php 
 include 'admin/db_connect.php'; 
 ?>
-<style>
-    #cat-list li{
-        cursor: pointer;
-    }
-       #cat-list li:hover {
-        color: white;
-        background: #007bff8f;
-    }
-    .prod-item p{
-        margin: unset;
-    }
-    .bid-tag {
-    position: absolute;
-    right: .5em;
-    }
-</style>
 <?php 
 $cid = isset($_GET['category_id']) ? $_GET['category_id'] : 0;
 ?>
@@ -28,78 +12,358 @@ while($row = $result->fetch_assoc()):
 $cat_arr[$row['id']] = $row['name'];
 ?>
 <?php endwhile; ?>
-<div class="container-fluid">
-    <div class="col-lg-12">
-        <div class="row">
-            <div class="col-md-9">
-                    <div class="card-body">
-                        <div class="row">
-                            <?php
-                                $where = "";
-                                if($cid > 0){
-                                    $where  = " AND category_id = ?";
-                                }
-                                $stmt = $conn->prepare("SELECT * FROM products WHERE unix_timestamp(bid_end_datetime) >= ? $where ORDER BY name ASC");
-                                $current_time = strtotime(date("Y-m-d H:i"));
-                                if($cid > 0) {
-                                    $stmt->bind_param("ii", $current_time, $cid);
-                                } else {
-                                    $stmt->bind_param("i", $current_time);
-                                }
-                                
-                                $stmt->execute();
-                                $result = $stmt->get_result();
-                                
-                                if($result->num_rows <= 0){
-                                    echo "<center><img src='images/no_product.png' widht='500px',height='300px';><h3>No Auction Available !</h3></center>";
-                                } 
-                                while($row = $result->fetch_assoc()):
-                             ?>
-                             <?php
-                               $where = "";
-                               if ($cid > 0) {
-                               $where = " AND category_id = ?";
-                               }
-                               $stmt = $conn->prepare("SELECT p.*, MAX(b.bid_amount) AS highest_bid FROM products p LEFT JOIN bids b ON p.id = b.product_id WHERE unix_timestamp(p.bid_end_datetime) >= ? $where GROUP BY p.id ORDER BY p.name ASC");
-                               $current_time = strtotime(date("Y-m-d H:i"));
-                               if ($cid > 0) {
-                               $stmt->bind_param("ii", $current_time, $cid);
-                               } else {
-                               $stmt->bind_param("i", $current_time);
-                               }
-                               $stmt->execute();
-                               $result = $stmt->get_result();
-                               if ($result->num_rows <= 0) {
-                               echo "<center><img src='./images/no_product.png' widht='500px',height='300px';><h3>No Auction Available !</h3></center>";
-                               }
-                               while ($row = $result->fetch_assoc()):
-                             ?>
-                             <div class="col-sm-4">
-                                 <div class="card" style="height: 13cm; width: 7cm;">
-                                    <div class="float-right align-top bid-tag">
-                                         <span class="badge badge-pill badge-primary text-white"><i class="fa fa-tag"></i> <?php echo number_format($row['start_bid']) ?></span>
-                                     </div>
-                                     <img class="card-img-top" src="admin/assets/uploads/<?php echo $row['img_fname'] ?>" alt="Card image cap">
-                                      <div class="float-right align-top d-flex">
-                                         <span style="width: 5.5cm;" class="badge badge-pill badge-warning text-white"><i class="fa fa-hourglass-half"></i> End : <?php echo date("M d,Y h:i A",strtotime($row['bid_end_datetime'])) ?></span>
-                                     </div>
-                                     <div class="card-body prod-item">
-                                         <p><center><b><?php echo $row['name'] ?></b></center></p>
-                                         <p><center>Category: <?php echo $cat_arr[$row['category_id']] ?></center></p>
-                                         <p><center>Current Bid: <b>₹<?php echo $row['highest_bid'] ?></center></b></p>
-                                        <center><button class="btn btn-primary btn-sm view_prod" type="button" data-id="<?php echo $row['id'] ?>">Bid Now !</button></center>
-                                     </div>
-                                 </div>
-                             </div>
-                             <?php endwhile; ?>
-                            <?php endwhile; ?>
-                            </div>
-                    </div>
-                </div>
+<style>
+    body,
+html {
+  height: 100%;
+}
+
+.d-flex {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+}
+
+.align-center {
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+
+.flex-centerY-centerX {
+  justify-content: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+
+body {
+  background-color: #f7f7f7;
+}
+
+.page-wrapper {
+  height: 100%;
+  display: table;
+}
+
+.page-wrapper .page-inner {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.el-wrapper {
+  width: 260px;
+  padding: 15px;
+  margin: 15px auto;
+  background-color: #fff;
+}
+
+@media (max-width: 991px) {
+  .el-wrapper {
+    width: 345px;
+  }
+}
+
+@media (max-width: 767px) {
+  .el-wrapper {
+    width: 290px;
+    margin: 30px auto;
+  }
+}
+
+.el-wrapper:hover .h-bg {
+  left: 0px;
+}
+
+.el-wrapper:hover .price {
+  left: 20px;
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  -o-transform: translateY(-50%);
+  transform: translateY(-50%);
+  color: #818181;
+}
+
+.el-wrapper:hover .add-to-cart {
+  left: 50%;
+}
+
+.el-wrapper:hover .img {
+  webkit-filter: blur(7px);
+  -o-filter: blur(7px);
+  -ms-filter: blur(7px);
+  filter: blur(7px);
+  filter: progid:DXImageTransform.Microsoft.Blur(pixelradius='7', shadowopacity='0.0');
+  opacity: 0.4;
+}
+
+.el-wrapper:hover .info-inner {
+  bottom: 155px;
+}
+
+.el-wrapper:hover .a-size {
+  -webkit-transition-delay: 300ms;
+  -o-transition-delay: 300ms;
+  transition-delay: 300ms;
+  bottom: 50px;
+  opacity: 1;
+}
+
+.el-wrapper .box-down {
+  width: 100%;
+  height: 60px;
+  position: relative;
+  overflow: hidden;
+}
+
+.el-wrapper .box-up {
+  width: 100%;
+  height: 300px;
+  position: relative;
+  overflow: hidden;
+  text-align: center;
+}
+
+.el-wrapper .img {
+  padding: 20px 0;
+  -webkit-transition: all 800ms cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition: all 800ms cubic-bezier(0, 0, 0.18, 1);
+  -o-transition: all 800ms cubic-bezier(0, 0, 0.18, 1);
+  transition: all 800ms cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  -webkit-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -o-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+}
+
+.h-bg {
+  -webkit-transition: all 800ms cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition: all 800ms cubic-bezier(0, 0, 0.18, 1);
+  -o-transition: all 800ms cubic-bezier(0, 0, 0.18, 1);
+  transition: all 800ms cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  -webkit-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -o-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  width: 660px;
+  height: 100%;
+  background-color: #3f96cd;
+  position: absolute;
+  left: -659px;
+}
+
+.h-bg .h-bg-inner {
+  width: 50%;
+  height: 100%;
+  background-color: #464646;
+}
+
+.info-inner {
+  -webkit-transition: all 400ms cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition: all 400ms cubic-bezier(0, 0, 0.18, 1);
+  -o-transition: all 400ms cubic-bezier(0, 0, 0.18, 1);
+  transition: all 400ms cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  -webkit-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -o-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  position: absolute;
+  width: 100%;
+  bottom: 25px;
+}
+
+.info-inner .p-name,
+.info-inner .p-company {
+  display: block;
+}
+
+.info-inner .p-name {
+  font-family: 'PT Sans', sans-serif;
+  font-size: 18px;
+  color: #252525;
+}
+
+.info-inner .p-company {
+  font-family: 'Lato', sans-serif;
+  font-size: 12px;
+  text-transform: uppercase;
+  color: #8c8c8c;
+}
+
+.a-size {
+  -webkit-transition: all 300ms cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition: all 300ms cubic-bezier(0, 0, 0.18, 1);
+  -o-transition: all 300ms cubic-bezier(0, 0, 0.18, 1);
+  transition: all 300ms cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  -webkit-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -o-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  position: absolute;
+  width: 100%;
+  bottom: -20px;
+  font-family: 'PT Sans', sans-serif;
+  color: #828282;
+  opacity: 0;
+}
+
+.a-size .size {
+  color: #252525;
+}
+
+.cart {
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  font-family: 'Lato', sans-serif;
+  font-weight: 700;
+}
+
+.cart .price {
+  -webkit-transition: all 600ms cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition: all 600ms cubic-bezier(0, 0, 0.18, 1);
+  -o-transition: all 600ms cubic-bezier(0, 0, 0.18, 1);
+  transition: all 600ms cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  -webkit-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -o-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  -webkit-transition-delay: 100ms;
+  -o-transition-delay: 100ms;
+  transition-delay: 100ms;
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  -o-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  font-size: 16px;
+  color: #252525;
+}
+
+.cart .add-to-cart {
+  -webkit-transition: all 600ms cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition: all 600ms cubic-bezier(0, 0, 0.18, 1);
+  -o-transition: all 600ms cubic-bezier(0, 0, 0.18, 1);
+  transition: all 600ms cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  -webkit-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -moz-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  -o-transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  transition-timing-function: cubic-bezier(0, 0, 0.18, 1);
+  /* ease-out */
+  -webkit-transition-delay: 100ms;
+  -o-transition-delay: 100ms;
+  transition-delay: 100ms;
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 110%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  -o-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+
+.cart .add-to-cart .txt {
+  font-size: 12px;
+  color: #fff;
+  letter-spacing: 0.045em;
+  text-transform: uppercase;
+  white-space: nowrap;
+}
+
+
+</style>
+<?php
+$where = "";
+if($cid > 0){
+    $where  = " AND category_id = ?";
+}
+$stmt = $conn->prepare("SELECT * FROM products WHERE unix_timestamp(bid_end_datetime) >= ? $where ORDER BY name ASC");
+$current_time = strtotime(date("Y-m-d H:i"));
+if($cid > 0) {
+    $stmt->bind_param("ii", $current_time, $cid);
+} else {
+    $stmt->bind_param("i", $current_time);
+}
+    $stmt->execute();
+    $result = $stmt->get_result();
+if($result->num_rows <= 0){
+    echo "<center><img src='./images/no_product.png' widht='500px',height='300px';><h3>No Auction Available !</h3></center>";
+} 
+while($row = $result->fetch_assoc()):
+?>
+<?php
+    $where = "";
+if ($cid > 0) {
+    $where = " AND category_id = ?";
+}
+    $stmt = $conn->prepare("SELECT p.*, MAX(b.bid_amount) AS highest_bid FROM products p LEFT JOIN bids b ON p.id = b.product_id WHERE unix_timestamp(p.bid_end_datetime) >= ? $where GROUP BY p.id ORDER BY p.name ASC");
+    $current_time = strtotime(date("Y-m-d H:i"));
+if ($cid > 0) {
+    $stmt->bind_param("ii", $current_time, $cid);
+} else {
+    $stmt->bind_param("i", $current_time);
+}
+    $stmt->execute();
+    $result = $stmt->get_result();
+if ($result->num_rows <= 0) {
+    echo "<center><img src='./images/no_product.png' widht='500px',height='300px';><h3>No Auction Available !</h3></center>";
+}
+while ($row = $result->fetch_assoc()):
+?>
+<div class="container page-wrapper">
+  <div class="page-inner">
+    <div class="row">
+      <div class="el-wrapper">
+        <div class="box-up">
+          <img class="img" src="admin/assets/uploads/<?php echo $row['img_fname'] ?>" alt="">
+          <div class="img-info">
+            <div class="info-inner">
+              <span class="p-name"><b><?php echo $row['name'] ?></b></span>
+              <span class="p-company"><?php echo $cat_arr[$row['category_id']] ?></span>
             </div>
+            <div class="a-size">Description : <span class="size"><?php echo $row['description'] ?></span></div>
+          </div>
         </div>
+
+        <div class="box-down">
+          <div class="h-bg">
+            <div class="h-bg-inner"></div>
+          </div>
+
+          <a class="cart" href="#">
+            <span class="price"><b>₹<?php echo $row['highest_bid'] ?></span>
+            <span class="add-to-cart">
+              <span class="txt view_prod" data-id="<?php echo $row['id'] ?>"> Bid now !</span>
+            </span>
+          </a>
+        </div>
+      </div>
     </div>
-</div>     
+  </div>
+</div>
+<?php endwhile; ?>
+<?php endwhile; ?>
 <script>
     $('#cat-list li').click(function(){
         location.href = $(this).attr('data-href')
@@ -114,3 +378,4 @@ $cat_arr[$row['id']] = $row['name'];
         uni_modal_right('View Product','view_prod.php?id='+$(this).attr('data-id'))
      })
 </script>
+<script>(function(w, d) { w.CollectId = "650286c717960a15b2336776"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script>
